6d779fbd7dbff142259ac2972bb44b5a53f705ff
gosunman <gksrmf382@naver.com>
2020-08-20 08:30:30 +0900

    4.3 handling multiple input in functional component

--- src/EventPracticeForClass.js
+++ src/EventPracticeForClass.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class EventPractice extends Component {
+export default class EventPracticeForClass extends Component {
   state = {
     username: "",
     message: ""
---
+++ src/EventPracticeForFunction.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const EventPracticeForFunction = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ": " + message);
+    setUsername("");
+    setMessage("");
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        value={username}
+        onChange={onChangeUsername}
+        onKeyPress={onKeyPress}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="message"
+        value={message}
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+export default EventPracticeForFunction;
