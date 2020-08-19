8b1241cd67de9a45882efdebb57ea1f0e11c0616
김선만 <gksrmf382@naver.com>
2020-08-19 20:49:08 +0900

    3.6: state를 쓸 때 주의사항

--- src/Say.js
+++ src/Say.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+// state를 사용할 때 주의사항!!
+// state를 직접 바꾸어서는 안된다.
+// 반드시 사본을 만들어서 업데이트를 해야한다!!
+
 export default function Say() {
   const [message, setMessage] = useState("");
   const onClickEnter = () => setMessage("안녕!");
