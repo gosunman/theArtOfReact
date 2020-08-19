357f2baf9d3d6c89a7dba48c542b08452c5edded
김선만 <gksrmf382@naver.com>
2020-08-19 20:37:49 +0900

    3.4.1.4: this.setState

--- src/Counter.js
+++ src/Counter.js
@@ -23,12 +23,35 @@ class Counter extends Component {
         <h1>{number}</h1>
         <h2>{fixedNumber}</h2>
         <button
+          // onClick={() => {
+          //   this.setState({ number: number + 1 });
+          //   this.setState({ number: this.state.number + 1 });
+          // }}
           onClick={() => {
-            this.setState({ number: number + 1 });
-            this.setState({ number: this.state.number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
           }}
         >
           +1
+        </button>
+        <button
+          // onClick={() => {
+          //   this.setState({ number: number + 1 });
+          //   this.setState({ number: this.state.number + 1 });
+          // }}
+          onClick={() => {
+            this.setState({ number: number - 1 }, () => {
+              console.log("-1 reduced");
+            });
+          }}
+        >
+          -1
         </button>
       </div>
     );
