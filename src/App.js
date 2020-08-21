ead7c841af678d4d6603e810b0e6854a8134427c
gosunman <gksrmf382@naver.com>
2020-08-21 13:19:07 +0900

    file rearrange

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+
 import "./styles.css";
 
 const App = () => {
-  return <div className="App">it's new</div>;
+  return (
+    <div className="App">
+      <h1>New App</h1>
+      <h1>Please, Work For Me</h1>
+    </div>
+  );
 };
 
 export default App;
