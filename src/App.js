ba7462315d9f3a0bbef7592dbe1322595192f9f4
gosunman <gksrmf382@naver.com>
2020-08-21 13:48:39 +0900

    8.2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./Counter";
+import Info from "./Info";
 
 import "./styles.css";
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="App">
       <h1>New App</h1>
       <h1>Please, Work For Me</h1>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "hide" : "show"}
+      </button>
+      {visible && <Info />}
       <Counter />
     </div>
   );
---
+++ src/Info.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+export default function Info() {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  });
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <b>name:</b> {name}
+        <b>nickname:</b> {nickname}
+      </div>
+    </div>
+  );
+}
