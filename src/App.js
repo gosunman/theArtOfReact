de7c54f6d7000903d0c1a4edc593cf26668619b7
gosunman <gksrmf382@naver.com>
2020-08-21 14:07:20 +0900

    8.3.1 Basic useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Counter from "./Counter";
 import Info from "./Info";
+import ForReducer from "./ForReducer";
 
 import "./styles.css";
 
@@ -11,6 +12,9 @@ const App = () => {
     <div className="App">
       <h1>New App</h1>
       <h1>Please, Work For Me</h1>
+      <hr />
+      <ForReducer />
+      <hr />
       <button
         onClick={() => {
           setVisible(!visible);
@@ -19,6 +23,7 @@ const App = () => {
         {visible ? "hide" : "show"}
       </button>
       {visible && <Info />}
+      <hr />
       <Counter />
     </div>
   );
---
+++ src/ForReducer.js
@@ -0,0 +1,39 @@
+import React, { useReducer } from "react";
+
+// useReducer를 사용했을 때의 가장 큰 장점은 컴포넌트 업데이트 로직을 컴포넌트 바깥으로 빼낼 수 있다는 것
+
+function reducer(state, action) {
+  // action.type에 따라 다른 작업 수행
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
+
+export default function ForReducer() {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+      <h1>The number is {state.value}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "INCREMENT" });
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "DECREMENT" });
+        }}
+      >
+        -1
+      </button>
+    </div>
+  );
+}
