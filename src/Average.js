a8b0bfa9b7dd95bf5353c04864b55817b4df7462
gosunman <gksrmf382@naver.com>
2020-08-21 14:58:24 +0900

    8.4 useMemo done

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 const getAverage = (numbers) => {
   console.log("calculating now");
@@ -10,14 +10,19 @@ const getAverage = (numbers) => {
 export default function Average() {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+
   const onChange = (e) => {
     setNumber(e.target.value);
   };
+
   const onInsert = (e) => {
     const nextList = list.concat(parseInt(number, 10));
     setList(nextList);
     setNumber("");
   };
+
+  const avg = useMemo(() => getAverage(list), [list]);
+
   return (
     <div>
       <h1>Average</h1>
@@ -29,8 +34,45 @@ export default function Average() {
         ))}
       </ul>
       <div>
-        <b>평균값:</b> {getAverage(list)}
+        <b>평균값:</b> {avg}
       </div>
     </div>
   );
 }
+
+// import React, { useState } from "react";
+
+// const getAverage = (numbers) => {
+//   console.log("calculating now");
+//   if (numbers.length === 0) return 0;
+//   const sum = numbers.reduce((a, b) => a + b);
+//   return sum / numbers.length;
+// };
+
+// export default function Average() {
+//   const [list, setList] = useState([]);
+//   const [number, setNumber] = useState("");
+//   const onChange = (e) => {
+//     setNumber(e.target.value);
+//   };
+//   const onInsert = (e) => {
+//     const nextList = list.concat(parseInt(number, 10));
+//     setList(nextList);
+//     setNumber("");
+//   };
+//   return (
+//     <div>
+//       <h1>Average</h1>
+//       <input value={number} onChange={onChange} />
+//       <button onClick={onInsert}>Enroll</button>
+//       <ul>
+//         {list.map((value, index) => (
+//           <li key={index}>{value}</li>
+//         ))}
+//       </ul>
+//       <div>
+//         <b>평균값:</b> {getAverage(list)}
+//       </div>
+//     </div>
+//   );
+// }
