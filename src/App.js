511cd0b761cf5ad2b3a36f645989e9b92c9b2b7c
sunman <gksrmf382@naver.com>
2020-08-23 09:44:49 +0900

    9.2.2 sass practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import "./styles.css";
+import SassComponent from "./SassComponent";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>New App</h1>
-      <h1>Please, Work For Me</h1>
-      <h3>Please</h3>
+    <div>
+      <SassComponent></SassComponent>
     </div>
   );
 };
